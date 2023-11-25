3875909ab7d49eee30d9df5471c0241c0571195c
Johannes Naylor <jonaylor89@gmail.com>
2023-11-25 18:56:39 -0500

    tapped toolkit

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ function Header() {
         <img src={logo} alt="logo" />
         &nbsp;&nbsp;&nbsp;
         <h2>
-          <b>tapped</b>
+          <b>tapped toolkit</b>
         </h2>
       </div>
       <button className="switch-theme-button" onClick={handleSwitchTheme}>
